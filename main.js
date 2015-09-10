7da2aa5708ab47f309e85303ebe73511bcaeadf1
Student <student@RU-019.local>
2015-09-10 12:23:53 -0600

    fourth done

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
-// var times = ["Five!", "Five!", "Five!"]
-// var tripleFive = function(word) {
-// 	for (var i = 0; i < 4; i++){
-// 		return word + word + word;
-// 		}
-// }
+var times = ["Five!", "Five!", "Five!"]
+var tripleFive = function(word) {
+	for (var i = 0; i < 4; i++){
+		return word + word + word;
+		}
+}
 // console.log(tripleFive("Five!"))
 
 // Not right but I succeeded woo hoo. 
@@ -11,11 +11,19 @@
 var lastLetter = function(string) {
 	return string.charAt(string.length -1)
 }
-console.log(lastLetter('hello'))
-console.log(lastLetter('island'))
+// console.log(lastLetter('hello'))
+// console.log(lastLetter('island'))
 
 var square = function(number) {
 	return number * number;
 }
-console.log(square(3))
-console.log(square(5))
+// console.log(square(3))
+// console.log(square(5))
+
+
+var negate = function(number) {
+	return -number;
+
+}
+console.log(negate(5))
+console.log(negate(-8))
