18e41929d0640114e23f18ed70eecd165306afcb
Student <student@RU-019.local>
2015-09-10 12:19:30 -0600

    second done

--- main.js
+++ main.js
@@ -1,9 +1,14 @@
-var times = ["Five!", "Five!", "Five!"]
-var tripleFive = function(word) {
-	for (var i = 0; i < 4; i++){
-		return word + word + word;
-		}
-}
-console.log(tripleFive("Five!"))
+// var times = ["Five!", "Five!", "Five!"]
+// var tripleFive = function(word) {
+// 	for (var i = 0; i < 4; i++){
+// 		return word + word + word;
+// 		}
+// }
+// console.log(tripleFive("Five!"))
 
 // Not right but I succeeded woo hoo. 
+
+var lastLetter = function(string) {
+	return string.charAt(string.length -1)
+}
+console.log(lastLetter('hello'))
